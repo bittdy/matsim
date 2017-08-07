dd7141c385b059847d24b0f3b3ad543ab07d01ea
gleich <gleich@192.168.35.111>
2017-08-07 14:49:21 +0200

    include timeDriven

--- playgrounds/vspshk/src/main/java/playground/gleich/av_bus/analysis/PtVehicleOperationCostAnalysis.java
+++ playgrounds/vspshk/src/main/java/playground/gleich/av_bus/analysis/PtVehicleOperationCostAnalysis.java
@@ -2,12 +2,8 @@ package playground.gleich.av_bus.analysis;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
-import java.util.TreeMap;
 
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
@@ -32,9 +28,11 @@ public class PtVehicleOperationCostAnalysis {
 	private static final String resultFileLines = FilePaths.PATH_BASE_DIRECTORY + "data/analysis/operationCost/operationCost_transitSchedule.100pct.base.perLine.csv";
 	
 	private final double costPerM = 0.0023;
+	private final double costPerS = 0.0;
 	
 	Map<Id<TransitRoute>, Id<TransitLine>> route2line = new HashMap<>();
 	Map<Id<TransitRoute>, Double> route2length = new HashMap<>();
+	Map<Id<TransitRoute>, Double> route2time = new HashMap<>();
 	Map<Id<TransitRoute>, Integer> route2numDepartures = new HashMap<>();
 	Map<Id<TransitRoute>, Double> route2cost = new HashMap<>();
 	
@@ -62,10 +60,13 @@ public class PtVehicleOperationCostAnalysis {
 	private void processTransitLine(TransitLine line) {
 		for (TransitRoute route: line.getRoutes().values()) {
 			double length = calculateLength(route);
+			double time = route.getStops().get(route.getStops().size() - 1).getDepartureOffset() - 
+					route.getStops().get(0).getArrivalOffset();
 			int numDepartures = route.getDepartures().size();
-			double cost = length * numDepartures * costPerM;
+			double cost = length * numDepartures * costPerM + time * numDepartures * costPerS;
 			route2line.put(route.getId(), line.getId());
 			route2length.put(route.getId(), length);
+			route2time.put(route.getId(), time);
 			route2numDepartures.put(route.getId(), numDepartures);
 			route2cost.put(route.getId(), cost);
 		}
@@ -94,14 +95,14 @@ public class PtVehicleOperationCostAnalysis {
 		try {
 			bw = IOUtils.getBufferedWriter(file);
 			// write header
-			bw.write("transitLineId" + sep + "transitRouteId" + sep + "length" + sep + "numDepartures" + sep +
+			bw.write("transitLineId" + sep + "transitRouteId" + sep + "length" + sep + "timeDriven" + sep + "numDepartures" + sep +
 					"cost");
 			bw.newLine();
 			for (TransitLine line: scenario.getTransitSchedule().getTransitLines().values()) {
 				for (TransitRoute route: line.getRoutes().values()) {
 					bw.write(route2line.get(route.getId()).toString() + sep + route.getId().toString() + sep +
-							route2length.get(route.getId()) + sep + route2numDepartures.get(route.getId()) +
-							sep + route2cost.get(route.getId()));
+							route2length.get(route.getId()) + sep + route2time.get(route.getId()) + sep + 
+							route2numDepartures.get(route.getId()) + sep + route2cost.get(route.getId()));
 					bw.newLine();
 				}
 			}
@@ -116,25 +117,31 @@ public class PtVehicleOperationCostAnalysis {
 		try {
 			bw = IOUtils.getBufferedWriter(file);
 			// write header
-			bw.write("transitLineId" + sep + "maxLength" + sep + "sumDepartures" + sep +
-					"sumDistanceDriven" + sep + "sumCost");
+			bw.write("transitLineId" + sep + "maxLength" + sep + "maxTimeDriven" + sep + "sumDepartures" + sep +
+					"sumDistanceDriven" + sep + "sumTimeDriven" + sep + "sumCost");
 			bw.newLine();
 
 			for (TransitLine line: scenario.getTransitSchedule().getTransitLines().values()) {
 				double maxLength = 0;
+				double maxTimeDriven = 0;
 				int sumDepartures = 0;
 				double sumDistanceDriven = 0;
+				double sumTimeDriven = 0;
 				double sumCost = 0;
 				for (TransitRoute route: line.getRoutes().values()) {
 					if (route2length.get(route.getId()) > maxLength) {
 						maxLength = route2length.get(route.getId());
 					}
+					if (route2time.get(route.getId()) > maxTimeDriven) {
+						maxTimeDriven = route2time.get(route.getId());
+					}
 					sumDepartures += route2numDepartures.get(route.getId());
 					sumDistanceDriven += route2numDepartures.get(route.getId()) * route2length.get(route.getId());
+					sumTimeDriven += route2numDepartures.get(route.getId()) * route2time.get(route.getId());
 					sumCost += route2cost.get(route.getId());
 				}
-				bw.write(line.getId().toString() + sep + maxLength + sep + sumDepartures +
-						sep + sumDistanceDriven + sep + sumCost);
+				bw.write(line.getId().toString() + sep + maxLength + sep + maxTimeDriven + sep + sumDepartures +
+						sep + sumDistanceDriven + sep + sumTimeDriven + sep + sumCost);
 				bw.newLine();
 			}
 			bw.close();
